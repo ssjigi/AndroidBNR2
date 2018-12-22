f6878510b413c5aeded6fb8b910db455dfbef121
Sungho Oh <ssjigi@naver.com>
2018-12-22 22:09:04 +0900

    add previous button

--- Ch02/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ Ch02/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -12,6 +12,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
+
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -78,6 +80,23 @@ public class QuizActivity extends AppCompatActivity {
                 showNextQuestion();
             }
         });
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPrevQuestion();
+            }
+        });
+
+        updateQuestion();
+    }
+
+    private void showPrevQuestion() {
+        mCurrentIndex--;
+        if (mCurrentIndex < 0) {
+            mCurrentIndex = mQuestionBank.length - 1;
+        }
         updateQuestion();
     }
 
