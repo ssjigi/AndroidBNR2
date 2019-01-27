b52dbe6c247a4954ee096d4f1e16c67a42cb5cce
Sungho Oh <ssjigi@naver.com>
2019-01-27 21:04:39 +0900

    resolve challenge of chapter 5

--- Ch05/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ Ch05/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -1,13 +1,35 @@
 package com.bignerdranch.android.geoquiz;
 
-public class Question {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Question implements Parcelable {
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mIsCheater;
 
     public Question(int textResId, boolean answerTrue) {
         mTextResId = textResId;
         mAnswerTrue = answerTrue;
     }
+
+    protected Question(Parcel in) {
+        mTextResId = in.readInt();
+        mAnswerTrue = in.readByte() != 0;
+        mIsCheater = in.readByte() != 0;
+    }
+
+    public static final Creator<Question> CREATOR = new Creator<Question>() {
+        @Override
+        public Question createFromParcel(Parcel in) {
+            return new Question(in);
+        }
+
+        @Override
+        public Question[] newArray(int size) {
+            return new Question[size];
+        }
+    };
 
     public int getTextResId() {
         return mTextResId;
@@ -23,5 +45,25 @@ public class Question {
 
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
+    }
+
+    public boolean isCheater() {
+        return mIsCheater;
+    }
+
+    public void setCheater(boolean cheater) {
+        mIsCheater = cheater;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mTextResId);
+        dest.writeByte((byte) (mAnswerTrue ? 1 : 0));
+        dest.writeByte((byte) (mIsCheater ? 1 : 0));
     }
 }
